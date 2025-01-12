88c4f5b6543e5d856e3887d0ee2e65cc781ea405
Alexandra Travkina <kodersha@icloud.com>
2025-01-12 20:56:47 +0700

    Update complete.sh

--- files/scripts/complete.sh
+++ files/scripts/complete.sh
@@ -2,6 +2,5 @@
 
 set -oue pipefail
 
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/rpmfusion-nonfree-nvidia-driver.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/google-chrome.repo
