46f89dd9c4b7d428c297a2ff995ac078cf87306e
Alexandra Travkina <kodersha@icloud.com>
2025-08-06 16:05:31 +0700

    Update

---
+++ files/scripts/complete.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/google-chrome.repo
---
+++ files/system/etc/profile.d/just.sh
@@ -0,0 +1,1 @@
+alias just='just --justfile /usr/share/ublue-os/just/60-custom.just'
