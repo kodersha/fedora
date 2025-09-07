f635fecc4cd95ea0c0dfb4ba329ab97134bfd0a7
Alexandra Travkina <kodersha@icloud.com>
2025-09-07 21:33:46 +0700

    Update complete.sh

--- files/scripts/complete.sh
+++ files/scripts/complete.sh
@@ -2,7 +2,7 @@
 
 set -oue pipefail
 
+rm -f /usr/share/doc/just/README*.md
+
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/google-chrome.repo
-
-rm -r /usr/share/doc/just/README*.md
