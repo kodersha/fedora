ba84e4779b134fcf21037a8b7a8cfde2513e0945
Alexandra Travkina <kodersha@icloud.com>
2025-09-20 02:56:49 +0700

    Update common-complete.sh

--- files/scripts/common-complete.sh
+++ files/scripts/common-complete.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
-# Rebuild kernel modules (e.g. NVIDIA) for the current kernel
-akmods --force --kernels "$(uname -r)"
-
 # Update initramfs for the current kernel only
 dracut -f "/boot/initramfs-$(uname -r).img" "$(uname -r)"
