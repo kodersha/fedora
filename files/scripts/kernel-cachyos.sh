c8c95a09ff8eaf88524b129f2d2ba51dbba31daf
Alexandra Travkina <kodersha@icloud.com>
2026-01-09 00:40:34 +0700

    Update kernel-cachyos.sh

--- files/scripts/kernel-cachyos.sh
+++ files/scripts/kernel-cachyos.sh
@@ -3,12 +3,6 @@
 # Tell this script to exit if there are any errors.
 set -oue pipefail
 
-LTS_BUILD=false
-
-if [ -f /tmp/ltsbuild ]; then
-    LTS_BUILD=true
-fi
-
 # Remove Fedora kernel & remove leftover files
 dnf5 -y remove kernel* && rm -r -f /usr/lib/modules/*
 
@@ -32,11 +26,7 @@ chmod +x 05-rpmostree.install 50-dracut.install
 popd
 
 # install kernel
-if $LTS_BUILD; then
-    dnf5 -y install --allowerasing kernel-cachyos-lts kernel-cachyos-lts-devel-matched
-else
-    dnf5 -y install --allowerasing kernel-cachyos kernel-cachyos-devel-matched
-fi
+dnf5 -y install --allowerasing kernel-cachyos kernel-cachyos-devel-matched
 
 pushd /usr/lib/kernel/install.d
 mv -f 05-rpmostree.install.bak 05-rpmostree.install
